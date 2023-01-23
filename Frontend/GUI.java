3b42816058e392689e76eb5b58fd7db5f9d92461
Hasnain Ali <hasnainali.2005@hotmail.com>
2023-01-22 22:32:10 -0500

    Updated GUI and unzipped all pokemon imgs

--- Frontend/GUI.java
+++ Frontend/GUI.java
@@ -4,16 +4,20 @@ import javax.swing.JFrame;
 public class GUI  {
     static CardLayout cl = new CardLayout();
     static JFrame panels = new JFrame();
+    static Player human = new Player();
+    static AI robot = new AI();
 
     public GUI() {
         panels.setSize(960, 573);
         panels.setLayout(cl);
         panels.add(new StartPanel().getStartPanel(), "Start Panel");
         panels.add(new BattlePanel().getBattlePanel(), "Battle Panel");
+        panels.add(new BagPanel().getBagPanel(), "Bag Panel");
         panels.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panels.setResizable(false);
         panels.setVisible(true);
         
+
 
     }
 
